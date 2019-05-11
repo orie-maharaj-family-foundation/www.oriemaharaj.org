0104533d314990e931a07792b2afbf9f16421e7e
Orie Maharaj Family Foundation <omfamilyfoundation@gmail.com>
2019-05-10 23:29:59 -0400

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Thank you for contacting us.We look forward to reading your comments. Kindly allow 48 hours before trying to reach us again. <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["submit"]; ?>
+</body>
+</html>
