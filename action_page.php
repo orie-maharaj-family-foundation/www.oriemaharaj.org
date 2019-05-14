f1675a276a393fc730bc7ecea8f943fb0279d756
Orie Maharaj Family Foundation <omfamilyfoundation@gmail.com>
2019-05-14 11:21:08 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -11,4 +11,5 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank you for contacting us, you should hear from us soon. Have a Wonderful Day";
   ?>
-
+</body>
+</html>
