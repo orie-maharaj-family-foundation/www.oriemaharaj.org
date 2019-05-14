c2a78546e06c1e9786030e4faa0c75aef71fe0de
Orie Maharaj Family Foundation <omfamilyfoundation@gmail.com>
2019-05-14 14:29:41 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,16 +1,16 @@
-<!DOCTYPE HTML>
-<html>
-<body>
-<? php $name = $_POST['name'];
+<? php 
+  if(insset($_POST['submit'])){
+$name = $_POST['name'];
 $email = $_POST['email'];
 $subject=$_POST['subject'];  
 $comment = $_POST['comment'];
 $formcontent="From: $name \n Comment: $comment";
-$recipient = "omfamilyfoundation@gmail.com";
+$mailTo = "omfamilyfoundation@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank you for contacting us, you should hear from us soon. Have a Wonderful Day";
-  ?>
-</body>
-</html>
+$headers = "From: $mailFrom";
+$txt="You have received an e-mail from ".$name.".\n\n".$message;
+mail($mailTo, $subject, $formcontent, $headers,$txt;
+header("Location: index.php?mailsend");
+}     
+     
+
